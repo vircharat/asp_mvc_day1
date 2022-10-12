76399c9c0273b4cf3cf1d9c1c49b65a64121544a
Chandan M <Chandan.M@valtech.com>
2022-10-12 18:21:15 +0530

    hii mvc

---
+++ BookTheShow/MovieCoreMvcUi/Controllers/MovieController.cs
@@ -0,0 +1,57 @@
+﻿using BookTheShowEntity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCoreMvcUi.Controllers
+{
+    public class MovieController : Controller
+    {   IConfiguration _configuration;
+        public MovieController(IConfiguration configuration)
+        {
+
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieEntry()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> MovieEntry(Moviev moviev)
+        {
+            ViewBag.status = "";
+            //using grabage collection only for inbuilt classes
+            using (HttpClient client=new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(moviev),Encoding.UTF8,"application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "Movie/AddMovie";//api controller name and its function
+
+                using(var response = await client.PostAsync(endPoint,content))
+                {
+                    if(response.StatusCode==System.Net.HttpStatusCode.OK)
+                    {   //dynamic viewbag we can create any variable name in run time
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "Movie Details Saved Successfull!!";
+                    }
+
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong Entries";
+                    }
+
+                }
+            }
+            return View();
+        }
+
+    }
+}
